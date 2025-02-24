ee20bc3ee5d031e9ed64e098d00f220cc636a446
OwenCochell <owencochell@gmail.com>
2025-02-24 18:52:08 -0500

    Model input arguments

--- cbmc/prvProcessIPPacket/prvProcessIPPacket_harness.c
+++ cbmc/prvProcessIPPacket/prvProcessIPPacket_harness.c
@@ -3,7 +3,10 @@
  * @brief TODO
  */
 
+#include <stdlib.h>
+
 #include "FreeRTOS.h"
+#include "FreeRTOS_IP.h"
 
 /**
  * @brief Starting point for formal analysis
@@ -11,4 +14,20 @@
  */
 void harness(void)
 {
+
+    // Model input arguments
+
+    IPPacket_t pack;
+
+    NetworkBufferDescriptor_t buff;
+
+    // Allocate data in buffer:
+
+    buff.pucEthernetBuffer = malloc(buff.xDataLength);
+
+    // Allocated data will not be NULL
+
+    __CPROVER_assume(buff.pucEthernetBuffer != NULL);
+
+    eFrameProcessingResult_t res = prvProcessIPPacket( &pack, &buff );
 }
