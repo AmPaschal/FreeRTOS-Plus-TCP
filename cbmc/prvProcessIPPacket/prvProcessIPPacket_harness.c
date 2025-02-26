e1077e9cf4f1f94d9d421add4320924c3bca0fde
OwenCochell <owencochell@gmail.com>
2025-02-26 10:18:35 -0500

    Stubbed some complicated functions

--- cbmc/prvProcessIPPacket/prvProcessIPPacket_harness.c
+++ cbmc/prvProcessIPPacket/prvProcessIPPacket_harness.c
@@ -8,6 +8,16 @@
 #include "FreeRTOS.h"
 #include "FreeRTOS_IP.h"
 
+BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxDescriptor, BaseType_t xReleaseAfterSend ) {
+    BaseType_t res;
+    return res;
+}
+
+BaseType_t xProcessReceivedTCPPacket( NetworkBufferDescriptor_t * pxDescriptor ) {
+    BaseType_t res;
+    return res;
+}
+
 /**
  * @brief Starting point for formal analysis
  * 
@@ -22,6 +32,8 @@ void harness(void)
     NetworkBufferDescriptor_t buff;
 
     // Allocate data in buffer:
+
+    __CPROVER_assume(buff.xDataLength < 10);
 
     buff.pucEthernetBuffer = malloc(buff.xDataLength);
 
