aa1da0b9a46ef62d61dc56fa54a9cfa98d816408
Paschal Amusuo <amusuopaschal@gmail.com>
2025-03-07 12:20:42 -0500

    Completed the harness. Removed unnecessary files

--- cbmc/eARPProcessPacket/eARPProcessPacket_harness.c
+++ cbmc/eARPProcessPacket/eARPProcessPacket_harness.c
@@ -37,41 +37,6 @@ NetworkBufferDescriptor_t * pxGetNetworkBufferWithDescriptor( size_t xRequestedS
 BaseType_t dummy(struct xNetworkInterface *pxDescriptor, NetworkBufferDescriptor_t *const pxNetworkBuffer, BaseType_t xReleaseAfterSend) {}
 
 /**
- * @brief Function to check whether the current context belongs to
- *        the IP-task.
- *
- * @return If the current context belongs to the IP-task, then pdTRUE is
- *         returned. Else pdFALSE is returned.
- *
- * @note Very important: the IP-task is not allowed to call its own API's,
- *        because it would easily get into a dead-lock.
- */
-BaseType_t xIsCallingFromIPTask(void)
-{
-
-    BaseType_t xReturn;
-
-    return xReturn;
-}
-
-/**
- * @brief Checks if the IP address matches the global 255.255.255.255 broadcast address or
- * the broadcast address for any of our IPv4 endpoints.
- *
- * @param[in] ulIPAddress The IP address being checked.
- *
- * @param[out] ppxEndPoint Pointer to an end-point where we store the endpoint whose broadcast address we matched. Or NULL if no IPv4 endpoints were found.
- *
- * @return pdTRUE if the IP address is a broadcast address or else, pdFALSE.
- */
-BaseType_t xIsIPv4Broadcast( uint32_t ulIPAddress, struct xNetworkEndPoint ** ppxEndPoint ) {
-
-    BaseType_t res;
-
-    return res;
-}
-
-/**
  * @brief Find the end-point which has a given IPv4 address.
  *
  * @param[in] ulIPAddress The IP-address of interest, or 0 if any IPv4 end-point may be returned.
@@ -110,7 +75,21 @@ NetworkEndPoint_t *FreeRTOS_FindEndPointOnIP_IPv4(uint32_t ulIPAddress)
 NetworkEndPoint_t *FreeRTOS_NextEndPoint(const NetworkInterface_t *pxInterface,
                                          NetworkEndPoint_t *pxEndPoint)
 {
-    return FreeRTOS_FindEndPointOnIP_IPv4(0);
+    NetworkEndPoint_t *pxEndPoint = (NetworkEndPoint_t*)malloc(sizeof(NetworkEndPoint_t));
+
+    if (pxEndPoint != NULL) {
+        // Allocate the network interface:
+
+        pxEndPoint->pxNetworkInterface = (NetworkInterface_t *)malloc(sizeof(NetworkInterface_t));
+
+        if (pxEndPoint->pxNetworkInterface != NULL) {
+            // Assign a dummy callback:
+
+            pxEndPoint->pxNetworkInterface->pfOutput = dummy;
+        }
+    }
+
+    return pxEndPoint;
 }
 
 /**
@@ -123,7 +102,21 @@ NetworkEndPoint_t *FreeRTOS_NextEndPoint(const NetworkInterface_t *pxInterface,
  */
 NetworkEndPoint_t *FreeRTOS_FirstEndPoint(const NetworkInterface_t *pxInterface)
 {
-    return FreeRTOS_FindEndPointOnIP_IPv4(0);
+    NetworkEndPoint_t *pxEndPoint = (NetworkEndPoint_t*)malloc(sizeof(NetworkEndPoint_t));
+
+    if (pxEndPoint != NULL) {
+        // Allocate the network interface:
+
+        pxEndPoint->pxNetworkInterface = (NetworkInterface_t *)malloc(sizeof(NetworkInterface_t));
+
+        if (pxEndPoint->pxNetworkInterface != NULL) {
+            // Assign a dummy callback:
+
+            pxEndPoint->pxNetworkInterface->pfOutput = dummy;
+        }
+    }
+
+    return pxEndPoint;
 }
 
 /**
@@ -135,7 +128,21 @@ NetworkEndPoint_t *FreeRTOS_FirstEndPoint(const NetworkInterface_t *pxInterface)
  */
 NetworkEndPoint_t *FreeRTOS_FindGateWay(BaseType_t xIPType)
 {
-    return FreeRTOS_FindEndPointOnIP_IPv4(0);
+    NetworkEndPoint_t *pxEndPoint = (NetworkEndPoint_t*)malloc(sizeof(NetworkEndPoint_t));
+
+    if (pxEndPoint != NULL) {
+        // Allocate the network interface:
+
+        pxEndPoint->pxNetworkInterface = (NetworkInterface_t *)malloc(sizeof(NetworkInterface_t));
+
+        if (pxEndPoint->pxNetworkInterface != NULL) {
+            // Assign a dummy callback:
+
+            pxEndPoint->pxNetworkInterface->pfOutput = dummy;
+        }
+    }
+
+    return pxEndPoint;
 }
 
 /**
@@ -147,7 +154,21 @@ NetworkEndPoint_t *FreeRTOS_FindGateWay(BaseType_t xIPType)
  */
 NetworkEndPoint_t *FreeRTOS_FindEndPointOnNetMask(uint32_t ulIPAddress)
 {
-    return FreeRTOS_FindEndPointOnIP_IPv4(0);
+    NetworkEndPoint_t *pxEndPoint = (NetworkEndPoint_t*)malloc(sizeof(NetworkEndPoint_t));
+
+    if (pxEndPoint != NULL) {
+        // Allocate the network interface:
+
+        pxEndPoint->pxNetworkInterface = (NetworkInterface_t *)malloc(sizeof(NetworkInterface_t));
+
+        if (pxEndPoint->pxNetworkInterface != NULL) {
+            // Assign a dummy callback:
+
+            pxEndPoint->pxNetworkInterface->pfOutput = dummy;
+        }
+    }
+
+    return pxEndPoint;
 }
 
 /**
@@ -159,19 +180,20 @@ void harness(void)
 
     // Model awaiting buffer:
 
-    NetworkBufferDescriptor_t buff;
+    NetworkBufferDescriptor_t *buff = malloc(sizeof(NetworkBufferDescriptor_t));
+    __CPROVER_assume(buff != NULL);
 
     // Size must be at lest ARPPacket_t
 
-    __CPROVER_assume(buff.xDataLength >= sizeof(ARPPacket_t));
+    __CPROVER_assume(buff->xDataLength >= sizeof(ARPPacket_t));
 
-    buff.pucEthernetBuffer = malloc(buff.xDataLength);
+    buff->pucEthernetBuffer = malloc(buff->xDataLength);
 
-    __CPROVER_assume(buff.pucEthernetBuffer != NULL);  // WIll NOT be NULL
+    __CPROVER_assume(buff->pucEthernetBuffer != NULL);  // WIll NOT be NULL
 
     // Properly allocate the px endpoint:
 
-    buff.pxEndPoint = FreeRTOS_FindEndPointOnIP_IPv4(0);
+    buff->pxEndPoint = FreeRTOS_FindEndPointOnIP_IPv4(0);
 
     // Model global buffer
 
@@ -192,5 +214,5 @@ void harness(void)
         __CPROVER_assume(pxARPWaitingNetworkBuffer->pucEthernetBuffer != NULL);
     }
 
-    eFrameProcessingResult_t res = eARPProcessPacket(&buff);
+    eFrameProcessingResult_t res = eARPProcessPacket(buff);
 }
