e72bf36a4a994554e2ce8ac825ca0de763a4d9ba
Paschal Amusuo <amusuopaschal@gmail.com>
2025-03-07 14:09:39 -0500

    Completed the harness

--- source/FreeRTOS_TCP_Reception.c
+++ source/FreeRTOS_TCP_Reception.c
@@ -267,8 +267,8 @@
                         lIndex = -1;
 
                         /* Return Condition found. */
-                        // Commenting to recreate CVE-2018-16523
-                        // xReturn = pdTRUE;
+                        // Comment out to recreate CVE-2018-16523
+                        xReturn = pdTRUE;
                     }
                     else
                     {
@@ -281,11 +281,11 @@
                 {
                     /* Restrict the minimum value of segment length to the ( Minimum IP MTU (576) - IP header(20) - TCP Header(20) ).
                      * See "RFC 791 section 3.1 Total Length" for more details. */
-                    // Commenting to recreate CVE-2018-16523
-                    // if( uxNewMSS < tcpMINIMUM_SEGMENT_LENGTH )
-                    // {
-                    //     uxNewMSS = tcpMINIMUM_SEGMENT_LENGTH;
-                    // }
+                    // Comment out to recreate CVE-2018-16523
+                    if( uxNewMSS < tcpMINIMUM_SEGMENT_LENGTH )
+                    {
+                        uxNewMSS = tcpMINIMUM_SEGMENT_LENGTH;
+                    }
 
                     if( pxSocket->u.xTCP.usMSS > uxNewMSS )
                     {
